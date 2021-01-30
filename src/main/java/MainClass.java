02aa5df8812b5149a227862567e277fead26bf27
Paruk1 <parail200111@gmail.com>
2021-01-30 10:15:40 +0200

    delete code in MainClass

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -11,20 +11,6 @@ public class MainClass {
     static WebDriver driver;
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","..\\githubTest\\src\\main\\resources\\chromedriver.exe");
 
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.get("https://github.com/join?return_to=%2Fenterprise&source=login");
-
-        SignUpPage signUpPage = new SignUpPage(driver);
-
-        SignUpPage signUpPage1 = signUpPage.enterUserName("test");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        String error = signUpPage1.getUserNameError();
-        System.out.println(error);
-        driver.quit();
-        Assert.assertEquals("Username test is not available.",error);
     }
 }
